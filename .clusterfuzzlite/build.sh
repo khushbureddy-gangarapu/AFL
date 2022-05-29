fc3177c3810fa852890f3aafaa830a254031d639
khushbureddy-gangarapu <104974373+khushbureddy-gangarapu@users.noreply.github.com>
2022-05-30 00:07:34 +0530

    Create build.sh

---
+++ .clusterfuzzlite/build.sh
@@ -0,0 +1,13 @@
+set -e
+CC=clang  CXX=clang++ CFLAGS=-fsanitize=address
+export CC CXX
+set $LLVM_CONFIG=/usr/bin/llvm-config-10
+
+git clone https://github.com/AFLplusplus/AFLplusplus
+
+cd AFLplusplus
+make all
+make install
+cp $SRC/.clusterfuzzlite/imgRead.c .
+
+./afl-cc -fsanitize=address,undefined -ggdb imgRead.c -o imgRead
