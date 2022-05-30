c467b850fdf40890ef5a35a92535fc12ab691e52
khushbureddy-gangarapu <104974373+khushbureddy-gangarapu@users.noreply.github.com>
2022-05-30 13:55:56 +0100

    Update build.sh

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,20 +1,24 @@
-# Build fuzzers into $OUT. These could be detected in other ways.
+#!/bin/bash -eu
+# Copyright 2020 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+
+# Build and install project (using current CFLAGS, CXXFLAGS).
+
+
+# Build fuzzers in $OUT.
 for fuzzer in $(find $SRC -name 'imgRead.c'); do
-  fuzzer_basename=$(basename -s .c $fuzzer)
-  fuzzer_package=${fuzzer_basename}.o
-
-  # Create execution wrapper. Atheris requires that certain libraries are
-  # preloaded, so this is also done here to ensure compatibility and simplify
-  # test case reproduction. Since this helper script is what OSS-Fuzz will
-  # actually execute, it is also always required.
-  # NOTE: If you are fuzzing python-only code and do not have native C/C++
-  # extensions, then remove the LD_PRELOAD line below as preloading sanitizer
-  # library is not required and can lead to unexpected startup crashes.
-  echo "#!/bin/sh
-
-this_dir=\$(dirname \"\$0\")
-LD_PRELOAD=\$this_dir/sanitizer_with_fuzzer.so \
-ASAN_OPTIONS=\$ASAN_OPTIONS:symbolize=1:external_symbolizer_path=\$this_dir/llvm-symbolizer:detect_leaks=0 \
-\$this_dir/$fuzzer_package \$@" > $OUT/$fuzzer_basename
-  chmod +x $OUT/$fuzzer_basename
+  compile_c_fuzzer $fuzzer
 done
