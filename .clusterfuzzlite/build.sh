abf0b961f9b00c821cfbbd26771f6f2a69c03044
khushbureddy-gangarapu <104974373+khushbureddy-gangarapu@users.noreply.github.com>
2022-05-31 11:00:13 +0100

    Update build.sh

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,7 +1,20 @@
+# Build fuzzers into $OUT. These could be detected in other ways.
 for fuzzer in $(find $SRC -name 'imgRead.c'); do
-  clang -fsanitize=address $fuzzer -o imgRead
-  ldd ./imgRead
-  cp imgRead $OUT/
-  cp $fuzzer $OUT/
-  chmod +x $OUT/
+  fuzzer_basename=$(basename -s .c $fuzzer)
+  fuzzer_package=${fuzzer_basename}.pkg
+
+  # Create execution wrapper. Atheris requires that certain libraries are
+  # preloaded, so this is also done here to ensure compatibility and simplify
+  # test case reproduction. Since this helper script is what OSS-Fuzz will
+  # actually execute, it is also always required.
+  # NOTE: If you are fuzzing python-only code and do not have native C/C++
+  # extensions, then remove the LD_PRELOAD line below as preloading sanitizer
+  # library is not required and can lead to unexpected startup crashes.
+  echo "#!/bin/sh
+# LLVMFuzzerTestOneInput for fuzzer detection.
+this_dir=\$(dirname \"\$0\")
+LD_PRELOAD=\$this_dir/sanitizer_with_fuzzer.so \
+ASAN_OPTIONS=\$ASAN_OPTIONS:symbolize=1:external_symbolizer_path=\$this_dir/llvm-symbolizer:detect_leaks=0 \
+\$this_dir/$fuzzer_package \$@" > $OUT/$fuzzer_basename
+  chmod +x $OUT/$fuzzer_basename
 done
