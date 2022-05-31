66c485a6e1f55f15037a9e762453b4d03e6dcd78
khushbureddy-gangarapu <104974373+khushbureddy-gangarapu@users.noreply.github.com>
2022-05-31 12:42:23 +0100

    Update build.sh

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,12 +1,8 @@
-# Build fuzzers into $OUT. These could be detected in other ways.
 for fuzzer in $(find $SRC -name 'imgRead.c'); do
-  fuzzer_basename=$(basename -s .c $fuzzer)
-  fuzzer_package=${fuzzer_basename}.pkg
- 
-  echo "#!/bin/sh
-# LLVMFuzzerTestOneInput for fuzzer detection.
-this_dir=\$(dirname \"\$0\")
-ASAN_OPTIONS=\$ASAN_OPTIONS:symbolize=1:external_symbolizer_path=\$this_dir/llvm-symbolizer:detect_leaks=0 \
-\$this_dir/$fuzzer_package \$@" > $OUT/$fuzzer_basename
-  chmod +x $OUT/$fuzzer_basename
+  clang -fsanitize=address $fuzzer -o imgRead
+  ldd ./imgRead
+  cp imgRead $OUT/
+  cp $fuzzer $OUT/
+  chmod +x $OUT/
+  ./imgRead &> outfile.txt
 done
